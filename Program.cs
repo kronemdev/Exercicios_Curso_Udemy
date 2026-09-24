86342240a1cd60919a5168f17b8395ee1d4724ea
Nicolas Kronemberger <kronemxd@gmail.com>
2026-09-23 23:06:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿//EXERCÍCIO 01:
+//Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+/*
+string nome1;
+string nome2;
+double idade1;
+double idade2;
+double resultado;
+
+Console.WriteLine("Dados Da Primeira Pessoa");
+Console.WriteLine("Nome : ");
+nome1 = Console.ReadLine();
+Console.WriteLine("Idade : ");
+idade1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Dados da segunda pessoa");
+Console.WriteLine("Nome : ");
+nome2 = Console.ReadLine();
+Console.WriteLine("Idade : ");
+idade2 = double.Parse(Console.ReadLine());
+
+if (idade1 > idade2)
+{
+    Console.WriteLine($"Pessoa mais velha :{nome1} ");
+}
+else
+{
+    Console.WriteLine($"Pessoa mais velha : {nome2}");
+}
+*/
+
+
+
+// -------------------------------------------------------------------------------------------
+
+// EXERCÍCIO 02:
+// Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+// médio dos funcionários.
+
+
+/*
+string nome1;
+double salario1;
+string nome2;
+double salario2;
+double resultado;
+
+Console.WriteLine("Dados do primeiro Funcionario");
+Console.WriteLine("Nome : ");
+nome1 = Console.ReadLine();
+Console.WriteLine("Salario : ");
+salario1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do segundo Funcionario");
+Console.WriteLine("Nome : ");
+nome2 = Console.ReadLine();
+Console.WriteLine("Salario : ");
+salario2 = double.Parse(Console.ReadLine());
+
+resultado = (salario1 + salario2) / 2;
+Console.WriteLine($"Salario medio = {resultado.ToString("F2")}");
+
+*/
+
+// -------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
